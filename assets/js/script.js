e495855dc750861b98f0810b3170318f7a0626f7
khaled <khaleda@gmail.com>
2019-05-23 13:34:16 +0600

    Saved

---
+++ assets/js/script.js
@@ -0,0 +1,61 @@
+(function($){
+
+
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    items:1,
+    autoplay:true,
+    smartSpeed : 3000
+});
+
+$( function() {
+    $( "#accordion" ).accordion();
+  } );
+// $( function() {
+// 	$( "#progressbar-90" ).progressbar({
+// 		value: 90,
+// 	});
+// 	$( "#progressbar-80" ).progressbar({
+// 		value: 80
+// 	});
+// 	$( "#progressbar-55" ).progressbar({
+// 		value: 55
+// 	});
+// 	$( "#progressbar-50" ).progressbar({
+// 		value: 50
+// 	})
+// })
+$( '#progressbar-90' ).LineProgressbar({
+	percentage: 90,
+	fillBackgroundColor: '#60ba60',
+	backgroundColor: '#EEEEEE',
+  	radius: '5px',
+  	height: '10px',
+  	width: '100%'
+})
+$( '#progressbar-80' ).LineProgressbar({
+	percentage: 80,
+	fillBackgroundColor: '#60c2df',
+	backgroundColor: '#EEEEEE',
+  	radius: '5px',
+  	height: '10px',
+  	width: '100%'
+})
+$( '#progressbar-55' ).LineProgressbar({
+	percentage: 55,
+	fillBackgroundColor: '#f0af53',
+	backgroundColor: '#EEEEEE',
+  	radius: '5px',
+  	height: '10px',
+  	width: '100%'
+})
+$( '#progressbar-50' ).LineProgressbar({
+	percentage: 50,
+	fillBackgroundColor: '#db5a57',
+	backgroundColor: '#EEEEEE',
+  	radius: '5px',
+  	height: '10px',
+  	width: '100%'
+})
+
+}(jQuery))
